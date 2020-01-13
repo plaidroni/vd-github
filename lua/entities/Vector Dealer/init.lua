23011bf9dfc43b507833469b4961b16c6f0dd196
tobiasafischer <tobias.alan.fischer@gmail.com>
2020-01-12 16:44:21 -0800

    Update init.lua

--- lua/entities/Vector Dealer/init.lua
+++ lua/entities/Vector Dealer/init.lua
@@ -36,6 +36,7 @@ function ENT:disappear()
                     self:SetPos(randomvec)
                     self:SetAngles(randomangle)
                     v:ScreenFade( SCREENFADE.IN, Color( 0,0,0,255 ), 3, 0 )
+                    self:AddGestureSequence(351,false)
                 end
                 timer.Simple(1,disappearinn)
         end
@@ -112,6 +113,11 @@ function ENT:Use( Name, Caller )
     end
 end
  
+
+
+
+
+
 net.Receive("vectordealer_BuyWeapon", function(len, ply, wepindex)
     wepindex = net.ReadInt(24)
     moneyamount = ply:getDarkRPVar("money")
@@ -133,6 +139,10 @@ net.Receive("vectordealer_BuyWeapon", function(len, ply, wepindex)
     end
    
 end)
+
+
+
+
 
 function grabPosAngle()
     posang = {}
